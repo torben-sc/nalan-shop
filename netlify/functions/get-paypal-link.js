915f1ac94d8db6f790333145b4c7a4c5fef28f08
torben-sc <torben.schmidt610@gmail.com>
2024-11-05 22:55:03 +0100

    feat: add thank you page

--- netlify/functions/get-paypal-link.js
+++ netlify/functions/get-paypal-link.js
@@ -3,13 +3,13 @@ exports.handler = async (event, context) => {
     // Hole die PayPal-Link Umgebungsvariable
     const paypalLink = process.env.PAYPAL_LINK;
   
-    if (!paypalLink) {
+    if (!paypalLink) { //hier eventuell weitere Sicherheitsmaßnahmen hinzufügen, bspw.: JSON Web Token oder URL Filter
       return {
         statusCode: 500,
         body: JSON.stringify({ error: 'PayPal-Link nicht gefunden. Bitte überprüfen Sie die Umgebungsvariablen.' }),
       };
     }
-  
+    
     return {
       statusCode: 200,
       body: JSON.stringify({ link: paypalLink }),
