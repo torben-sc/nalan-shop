f002879e83389fbba8b8bb2c6704555978e35cb9
torben-sc <torben.schmidt610@gmail.com>
2024-11-05 21:22:31 +0100

    Add Netlify serverless function to provide PayPal link

--- netlify/functions/get-paypal-link.js
+++ netlify/functions/get-paypal-link.js
@@ -1,6 +1,18 @@
+// netlify/functions/get-paypal-link.js
 exports.handler = async (event, context) => {
+    // Hole die PayPal-Link Umgebungsvariable
+    const paypalLink = process.env.PAYPAL_LINK;
+  
+    if (!paypalLink) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'PayPal-Link nicht gefunden. Bitte überprüfen Sie die Umgebungsvariablen.' }),
+      };
+    }
+  
     return {
       statusCode: 200,
-      body: JSON.stringify({ link: process.env.PAYPAL_LINK })
+      body: JSON.stringify({ link: paypalLink }),
     };
   };
+  
