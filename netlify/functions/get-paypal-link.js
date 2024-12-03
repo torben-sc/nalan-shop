744e469e727ae65300defac31db8a1fa21573757
torben-sc <torben.schmidt610@gmail.com>
2024-12-03 19:29:20 +0100

    fix: securing paypal link using netlify

--- netlify/functions/get-paypal-link.js
+++ netlify/functions/get-paypal-link.js
@@ -1,35 +1,34 @@
 // netlify/functions/get-paypal-link.js
 
 exports.handler = async (event, context) => {
-  // Hole die Produkt-ID aus den URL-Parametern
   const productId = event.queryStringParameters.productId;
 
   if (!productId) {
-      return {
-          statusCode: 400,
-          body: JSON.stringify({ error: 'Produkt-ID fehlt.' }),
-      };
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'Produkt-ID fehlt.' }),
+    };
   }
 
-  // Hole die PayPal-Link-Umgebungsvariable basierend auf der Produkt-ID
   const paypalLinkKey = `PAYPAL_LINK_PRODUCT${productId.toUpperCase()}`;
   const paypalLink = process.env[paypalLinkKey];
 
   if (!paypalLink) {
-      return {
-          statusCode: 500,
-          headers: {
-              "Access-Control-Allow-Origin": "*", // Diese Zeile ermöglicht Cross-Origin-Anfragen
-          },
-          body: JSON.stringify({ error: 'PayPal-Link nicht gefunden. Bitte überprüfen Sie die Umgebungsvariablen.' }),
-      };
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Optional für spätere API-Anfragen
+      },
+      body: JSON.stringify({ error: 'PayPal-Link nicht gefunden.' }),
+    };
   }
 
+  // HTTP 302 Redirect zum PayPal-Link
   return {
-      statusCode: 302, // Status-Code für Weiterleitung
-      headers: {
-          "Location": paypalLink, // Weiterleitungsziel
-          "Access-Control-Allow-Origin": "*", // Cross-Origin-Anfragen ermöglichen
-      },
+    statusCode: 302,
+    headers: {
+      "Location": paypalLink,
+      "Access-Control-Allow-Origin": "*", // Optional, nicht notwendig für Redirects
+    },
   };
 };
