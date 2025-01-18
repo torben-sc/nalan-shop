8bcb23466067f8886fcf34a8f7455dcc9344c55a
torben-sc <torben.schmidt610@gmail.com>
2025-01-18 20:11:29 +0100

    feat: colors filter on product page

--- app.js
+++ app.js
@@ -194,24 +194,26 @@ async function displayProductList(category = null, size = null, accs = null) {
                     <img src="${product.defaultImage || product.variants[0].images[0]}" alt="${product.name}">
                     <h2>${product.name}</h2>
                 </a>
-                <p class="product-price-shop">
-                    ${
-                        product.variants.some(variant => variant.stock > 0) 
-                        ? `<span class="price-amount-shop">${product.price}</span><span class="price-currency-shop"> €</span>` 
-                        : `<span class="sold-out-text">SOLD OUT</span>`
-                    }
-                </p>
-                <div class="variant-colors-container">
-                    ${product.variants.map(variant => {
-                        const isSoldOut = variant.stock === 0;
-                        return `
-                            <span 
-                                class="variant-color ${isSoldOut ? 'sold-out' : ''}" 
-                                style="background-color: ${variant.color};" 
-                                title="${isSoldOut ? 'Sold Out' : 'Available'}">
-                            </span>
-                        `;
-                    }).join('')}
+                <div class="product-price-and-colors">
+                    <p class="product-price-shop">
+                        ${
+                            product.variants.some(variant => variant.stock > 0) 
+                            ? `<span class="price-amount-shop">${product.price}</span><span class="price-currency-shop"> €</span>` 
+                            : `<span class="sold-out-text">SOLD OUT</span>`
+                        }
+                    </p>
+                    <div class="variant-colors-inline">
+                        ${product.variants.map(variant => {
+                            const isSoldOut = variant.stock === 0;
+                            return `
+                                <span 
+                                    class="variant-color ${isSoldOut ? 'sold-out' : ''}" 
+                                    style="background-color: ${variant.color};" 
+                                    title="${isSoldOut ? 'Sold Out' : 'Available'}">
+                                </span>
+                            `;
+                        }).join('')}
+                    </div>
                 </div>
             `;
         } else {
