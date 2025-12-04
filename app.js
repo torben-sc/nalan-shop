ce3136abd30ad5c9456f8637d69b5397d77317a5
torben-sc <torben.schmidt610@gmail.com>
2025-12-04 21:07:19 +0100

    refactor: style changes

--- app.js
+++ app.js
@@ -510,7 +510,7 @@ function displayProductInfo(product, selectedVariant = null) {
                     or 
                     <a href="mailto:nalancreations@gmx.de" style="color: #E55013; text-decoration: none;">Email</a>
                 </div>
-                <br><br>
+                <br>
             `;
         } else {
             displayPrice = 'SOLD OUT';
